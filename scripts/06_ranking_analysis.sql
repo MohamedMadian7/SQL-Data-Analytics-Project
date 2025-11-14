f33a362447cec03ede540717f60476b37a4794ba
Mohamed Madian <mosamehmadian88@gmail.com>
2025-11-14 20:12:11 +0200

    Update 06_ranking_analysis.sql

--- scripts/06_ranking_analysis.sql
+++ scripts/06_ranking_analysis.sql
@@ -16,10 +16,10 @@ SQL Functions Used:
 -- Simple Ranking
 SELECT TOP 5
     p.product_name,
-    SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+    SUM(s.sales_amount) AS total_revenue
+FROM gold.fact_sales s
 LEFT JOIN gold.dim_products p
-    ON p.product_key = f.product_key
+    ON p.product_key = s.product_key
 GROUP BY p.product_name
 ORDER BY total_revenue DESC;
 
@@ -28,11 +28,11 @@ SELECT *
 FROM (
     SELECT
         p.product_name,
-        SUM(f.sales_amount) AS total_revenue,
-        RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
-    FROM gold.fact_sales f
+        SUM(s.sales_amount) AS total_revenue,
+        RANK() OVER (ORDER BY SUM(s.sales_amount) DESC) AS rank_products
+    FROM gold.fact_sales s
     LEFT JOIN gold.dim_products p
-        ON p.product_key = f.product_key
+        ON p.product_key = s.product_key
     GROUP BY p.product_name
 ) AS ranked_products
 WHERE rank_products <= 5;
@@ -40,10 +40,10 @@ WHERE rank_products <= 5;
 -- What are the 5 worst-performing products in terms of sales?
 SELECT TOP 5
     p.product_name,
-    SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+    SUM(s.sales_amount) AS total_revenue
+FROM gold.fact_sales s
 LEFT JOIN gold.dim_products p
-    ON p.product_key = f.product_key
+    ON p.product_key = s.product_key
 GROUP BY p.product_name
 ORDER BY total_revenue;
 
@@ -52,10 +52,10 @@ SELECT TOP 10
     c.customer_key,
     c.first_name,
     c.last_name,
-    SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+    SUM(s.sales_amount) AS total_revenue
+FROM gold.fact_sales s
 LEFT JOIN gold.dim_customers c
-    ON c.customer_key = f.customer_key
+    ON c.customer_key = s.customer_key
 GROUP BY 
     c.customer_key,
     c.first_name,
@@ -68,9 +68,9 @@ SELECT TOP 3
     c.first_name,
     c.last_name,
     COUNT(DISTINCT order_number) AS total_orders
-FROM gold.fact_sales f
+FROM gold.fact_sales s
 LEFT JOIN gold.dim_customers c
-    ON c.customer_key = f.customer_key
+    ON c.customer_key = s.customer_key
 GROUP BY 
     c.customer_key,
     c.first_name,
